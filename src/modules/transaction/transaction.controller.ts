9e4ab3d93d6dd09b4be37d86bdb75ff0bf46af6d
AakashRaviSankar <aakash.sankar@talentakeaways.com>
2025-05-25 14:27:34 +0530

    dd

--- src/modules/transaction/transaction.controller.ts
+++ src/modules/transaction/transaction.controller.ts
@@ -3,17 +3,20 @@ import { TransactionService } from './transaction.service';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
 import { TransactionStatus } from './entities/transaction.entity';
 import { ApiTags } from '@nestjs/swagger';
+import { Public } from 'common/decorators/public.decorator';
 
 @ApiTags('transactions')
 @Controller('v1/transactions')
+
 export class TransactionController {
   constructor(private readonly transactionService: TransactionService) {}
-
+    @Public()
   @Post()
   async createTransaction(@Body() createTransactionDto: CreateTransactionDto) {
     return this.transactionService.createTransaction(createTransactionDto);
   }
 
+    @Public()
   @Post(':id/status')
   async updateTransactionStatus(
     @Param('id') id: number,
@@ -22,11 +25,13 @@ export class TransactionController {
     return this.transactionService.updateTransactionStatus(id, status);
   }
 
+    @Public()
   @Get()
   async findAll() {
     return this.transactionService.findAll();
   }
 
+    @Public()
   @Get('/user/:userId')
   async findByUser(@Param('userId') userId: number) {
     return this.transactionService.findByUser(userId);
